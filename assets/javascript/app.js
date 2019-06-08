1294a615ae3e2f57c1760f85fa23100795dc85da
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-08 18:06:05 -0400

    file upload! :)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,88 @@
+
 var authors = [
     "Jack Canfield", "Brian Tracy", "Mark Victor Hansen", "John Maxwell", "Bob Proctor", "John Assaraf", "Napoleon Hill", "W. Clement Stone", "Philip C. McGraw", "Joe Vitale"
 ];
 
-$("#find-authors").on("click", function(event)  {
+function renderButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
 
-    event.preventDefault();
+    for (var i = 0; i < arrayToUse.length; i++) {
+        var best = $("<button");
+        best.addClass(classToAdd);
+        best.attr("data-type", arrayToUse[i]);
+        best.text(arrayToUse[i]);
+        $(areaToAddTo).append(best)
+    }
 
-    var Bob = $("#author-input").val();
-
-    $("#author-input").val("");
-    authors.push(Bob.trim());
-    renderButtons();
-});
-
-function renderButtons()    {
-        $("#authorButtons").empty();
-            for (i = 0; i < authors.length; i++)    {
-                $("#authorButtons").append("<button class='add authors btn btn-primary' style='margin:10px;'>" +authors[i] + "</button>")
-            }
 }
 
-$(document).on("click", ".add-foods", getauthorInfoFromApi);
+$(document).on("click", ".author-button", function()    {
+    $("#authors").empty();
+    $(".author-button").removeClass("active");
+    $(this).addClass("active");
 
-function getauthorInfoFromApi() {
-    var authorInfo  = "";
-    var queryURL    = ""
+    var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+
     $.ajax({
-            url: 'api.giphy.com/v1/gifs/search?api_key=lLUfXezLB8DLZldBrzJ2XY7t5u6PN24M?q="Bob Proctor"', 
-            method: 'GET'}).done(function(response){displayAuthorInfo(response);    }
-}
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response)    {
+            var results = response.data;
 
-renderButtons();
+            for (var i = 0; i < results.lenghth; i++)   {
+                var animalDiv = $("<div class=\"author-item\">");
 
-var myID = 0;
+                var rating = results[i].rating;
 
-function displayAuthorInfo(response)    {
-    $("#author-view").empty();
-    var imageArray = response.data;
-    var myHTML = "";
-    for (i = 0; i < imageArray.length; i++) {
-    var rating= imageArray[i].rating;
-    if
-    }
-}
+                var p = $("<p>").text("Rating: " + rating);
 
-$(document.body).on("click", "img", function()  {
-    var isAnimated = $(this).attr("isAnimated");
-    if (isAnimated ==null || isAnimated=='' )   {
-        isAnimated = "false";
-    }
+                var animated = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
 
-if(isAnimated=="true"){
-    $(this).attr("src", $(this).attr("stillSrc"));
-    $(this).attr("isAnimated", "false");
+                var animalImage = $("<img>");
+                animalImage.attr("src", still);
+                animalImage.attr("data-still", still);
+                animalImage.attr("data-animate", animated);
+                animalImage.attr("data-state", "still");
+                animalImage.addClass("animal-image");
 
-} else{
-    $(this).attr("src", $(this).attr("animatedSrc"));
-    $(this).attr("isAnimated", "true");
-}
+                animalDiv.append(paragraph);
+                animalDiv.append(animalImage);
+            }
+        });
+    });
+
+    $(document).on("click", ".author-image", function() {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still")  {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else{
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+    $("#add-author").on("click", function(event)    {
+        event.preventDefault();
+        var newAnimal = $("input").eq(0).val();
+
+        if (newAnimal.length > 2)   {
+            animals.push(newAnmial);
+        }
+
+        renderButtons(animals, "author-button", "#author-buttons");
+
+    });
+                
+    renderButtons(animals, "author-button", "#author-buttons");
+
 });
+
+
+
+  
